f7dfa15209b495f49a510058fdfd3bfb13e87369
Adityaverma0001 <limbaaditya2@gmail.com>
2025-08-16 11:22:49 +0530

    fix: css import case issue in slider.jsx

--- my-app/src/slider.jsx
+++ my-app/src/slider.jsx
@@ -6,7 +6,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
-import "./app.css"; 
+import "./App.css"; 
 import { motion } from "framer-motion";
 
 
